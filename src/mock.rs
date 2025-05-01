28da8e8139d721f834aedf9ba89f0df125f78963
Mostafa <mostafa.sedaghat@gmail.com>
2025-05-01 15:47:24 +0800

    refactor: remove add/ack channels

--- src/mock.rs
+++ src/mock.rs
@@ -1,7 +1,6 @@
 use super::bus::StreamBus;
+use crate::entry::Entry;
 use crate::error::Result;
-use crate::stream::Stream;
-use futures::channel::mpsc::Sender;
 use mockall::mock;
 
 mock! {
@@ -9,12 +8,7 @@ mock! {
 
     #[async_trait::async_trait]
     impl StreamBus for StreamBus1{
-        fn xadd_sender(&self) -> Sender<Stream>;
-        fn xack_sender(&self) -> Sender<Stream>;
-        async fn run<'a, 'b>(
-            &mut self,
-            keys: &[&'a str],
-            read_tx: &'b mut Sender<Stream>,
-        ) -> Result<()>;
+        async fn xadd(&mut self, entry: Entry) -> Result<String>;
+        async fn xack(&mut self, entry: &Entry) -> Result<String>;
     }
 }
